9ed5e58aff0a721ea1a458bee34adedd2374c8e7
Milton Palmeira <lpsantana_@hotmail.com>
2024-03-28 12:19:08 -0300

    first commit

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,9 @@
+export default function SignIn() {
+    return (
+      <div>
+        <h1>SignIn</h1>
+        <p>SignIn Page</p>
+      </div>
+    );
+  }
+  
---
+++ src/app/about/board/page.tsx
@@ -0,0 +1,8 @@
+export default function Board() {
+  return (
+    <div>
+      <h1>Board</h1>
+      <p>Board Page</p>
+    </div>
+  );
+}
