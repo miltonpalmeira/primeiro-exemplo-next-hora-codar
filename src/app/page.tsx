e6d82b7af2596a460869abb8a33dd9209067c9b6
miltonpalmeira <lpsantana_@hotmail.com>
2024-04-04 11:09:19 -0300

    links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+      <Link href="/products" style={{ fontSize: '30px', color: 'blue', textDecoration: 'underline' }}>Produtos</Link>
+
       <h1>Hello World!</h1>
-      <p>Welcome to NextJS.</p>
     </div>
   );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Products() {
     return (
       <div>
         <h1>Produtos</h1>
+        <Link href="/products" style={{ fontSize: '30px', color: 'blue', textDecoration: 'underline' }}>Voltar</Link>
       </div>
     );
   }
